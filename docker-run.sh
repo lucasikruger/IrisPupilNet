3d52cd5eeb7cfbff42dadc1d4cca3e83b03fc067
lucasikruger <lucasikruger@gmail.com>
2025-11-19 01:59:29 -0300

    improve docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -4,7 +4,7 @@
 set -e
 
 IMAGE_NAME="irispupilnet:latest"
-DATA_PATH="${1:-/data}"  # First argument or default to /data
+DATA_PATH="${1:-/media/hdd2/datasets/eyes}"  # First argument or default to /media/hdd2/datasets/eyes
 
 echo "Running Docker container: $IMAGE_NAME"
 echo "Mounting data from: $DATA_PATH"
